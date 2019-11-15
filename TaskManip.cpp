f8fc6bd764813aa026ac2e70ee16d0162c875ef1
AndreaHabib <54924600+AndreaHabib@users.noreply.github.com>
2019-11-15 17:33:22 -0500

    Update TaskManip.cpp

--- TaskManip.cpp
+++ TaskManip.cpp
@@ -132,151 +132,145 @@ void task::taskDone() {
 	}
 }
 void task::edit() {
-	system("CLS");
 	if (counter == 0) {
 		cout << "No tasks found!" << endl;
 	}
 	else {
-		cout << flush;
+		system("CLS");
 		string loop = "y";
-		int taskNum = counter + 1;
 		int end = 0;
 		while (loop == "y" || loop == "Y" || loop == "Yes" || loop == "YES" || loop == "yes" || loop == "yES" || loop == "YeS" && end != -1) {
+			int taskNum = counter + 1;
 			for (int i = 0; i < counter; i++) {
 				cout << "Task " << i + 1 << ". " << list[i].title << endl;
 			}
-			while (!cin || taskNum > counter || taskNum < 1) {
+			while (!cin || taskNum > counter || taskNum < 1 && taskNum != -1) {
+				cout << flush;
 				cout << "Please enter which task you would like edit: (from 1 to " << counter << ") (-1 to exit) ";
 				cin >> taskNum;
-				switch (taskNum) {
-				case -1:
-					end += -1;
-					break;
-				}
 				if (cin && taskNum > counter || taskNum < 1 && taskNum != -1) {
 					cin.clear();
 					cout << "Task not found! Please enter a valid task!" << endl;
 					cin.ignore();
 				}
 			}
-			switch (end) {
-			case -1:
-				break;
-			}
-			cout << "Which part would you like to edit?" << endl;
-			cout << "1. Title\n" << "2. Details\n" << "3. Date\n" << "4. Priority" << endl;
-			int part = 5;
-			while (!cin || part > 4 || part < 1 && part != -1) {
-				cout << "Please enter a number between 1 and 4: (-1 to exit)";
-				cin >> part;
-				if (cin && part > 4 || part < 1 && part != -1) {
-					cin.clear();
-					cout << "Invalid! Please enter a valid number!" << endl;
-					cin.ignore();
-				}
-			}
-			switch (part) {
-			case 1:
-				cout << "Old Title: " << list[taskNum - 1].title << endl;
-				cout << "New Title: ";
-				cin.ignore();
-				getline(cin, list[taskNum - 1].title);
-				break;
-			case 2:
-				cout << "Old Details: " << list[taskNum - 1].detail << endl;
-				cout << "New Detials: ";
-				cin.ignore();
-				getline(cin, list[taskNum - 1].detail);
-				break;
-			case 3:
-				printf("Old Due date: %d/%d/%d \n", list[taskNum - 1].day.month, list[taskNum - 1].day.day, list[taskNum - 1].day.year);
-				int mm, dd, yyyy;
-				int numOfDays;
-				GetLocalTime(&today);
-				list[taskNum - 1].day.day = 33;
-				list[taskNum - 1].day.year = 2018;
-				list[taskNum - 1].day.month = 13;
-				while (list[taskNum - 1].day.year < today.wYear || list[taskNum - 1].day.month > 12 || list[taskNum - 1].day.month < 1 || list[taskNum - 1].day.day > numOfDays || list[taskNum - 1].day.day < 1) {
-					cout << " New Due date: (MM/DD/YYYY)" << endl;
-					int i = 1;
-					while (i != 3) {
-						i = scanf_s("%d/%d/%d", &mm, &dd, &yyyy);
-						if (i != 3) {
-							cout << "Please enter in the correct format!" << endl;
-							cout << "Due date: (MM/DD/YYYY)" << endl;
-							fflush(stdin);
-							fseek(stdin, 0, SEEK_END);
-						}
-					}
-					list[taskNum - 1].day.day = dd;
-					list[taskNum - 1].day.year = yyyy;
-					list[taskNum - 1].day.month = mm;
-					switch (list[taskNum - 1].day.month) {
-					case -1:
-						break;
-					case 1:
-						numOfDays = 31;
-						break;
-					case 2:
-						if ((list[taskNum - 1].day.year % 4 == 0 && list[taskNum - 1].day.year % 100 != 0) || list[taskNum - 1].day.year % 400 == 0)
-							numOfDays = 29;
-						else numOfDays = 28;
-						break;
-					case 3:
-						numOfDays = 31;
-						break;
-					case 4:
-						numOfDays = 30;
-						break;
-					case 5:
-						numOfDays = 31;
-						break;
-					case 6:
-						numOfDays = 30;
-						break;
-					case 7:
-						numOfDays = 31;
-						break;
-					case 8:
-						numOfDays = 31;
-						break;
-					case 9:
-						numOfDays = 30;
-						break;
-					case 10:
-						numOfDays = 31;
-						break;
-					case 11:
-						numOfDays = 30;
-						break;
-					case 12:
-						numOfDays = 31;
-						break;
-					default:
-						cout << "Invalid month!" << endl;
-						break;
-					}
-					if (list[taskNum - 1].day.day > numOfDays || list[taskNum - 1].day.day < 1) {
+			loop = 'n';
+			if (taskNum != -1) {
+				cout << "Which part would you like to edit?" << endl;
+				cout << "1. Title\n" << "2. Details\n" << "3. Date\n" << "4. Priority" << endl;
+				int part = 5;
+				while (!cin || part > 4 || part < 1 && part != -1) {
+					cout << "Please enter a number between 1 and 4: (-1 to exit)";
+					cin >> part;
+					if (cin && part > 4 || part < 1 && part != -1) {
 						cin.clear();
-						cout << "Invalid day for the month and year you entered!" << endl;
+						cout << "Invalid! Please enter a valid number!" << endl;
 						cin.ignore();
 					}
-					if (list[taskNum - 1].day.year < today.wYear) {
-						cout << "Please enter a year within or after " << today.wYear << endl;
+				}
+				switch (part) {
+				case -1:
+					break;
+				case 1:
+					cout << "Old Title: " << list[taskNum - 1].title << endl;
+					cout << "New Title: ";
+					cin.ignore();
+					getline(cin, list[taskNum - 1].title);
+					break;
+				case 2:
+					cout << "Old Details: " << list[taskNum - 1].detail << endl;
+					cout << "New Detials: ";
+					cin.ignore();
+					getline(cin, list[taskNum - 1].detail);
+					break;
+				case 3:
+					printf("Old Due date: %d/%d/%d \n", list[taskNum - 1].day.month, list[taskNum - 1].day.day, list[taskNum - 1].day.year);
+					int mm, dd, yyyy;
+					int numOfDays;
+					GetLocalTime(&today);
+					list[taskNum - 1].day.day = 33;
+					list[taskNum - 1].day.year = 2018;
+					list[taskNum - 1].day.month = 13;
+					while (list[taskNum - 1].day.year < today.wYear || list[taskNum - 1].day.month > 12 || list[taskNum - 1].day.month < 1 || list[taskNum - 1].day.day > numOfDays || list[taskNum - 1].day.day < 1) {
+						cout << " New Due date: (MM/DD/YYYY)" << endl;
+						int i = 1;
+						while (i != 3) {
+							i = scanf_s("%d/%d/%d", &mm, &dd, &yyyy);
+							if (i != 3) {
+								cout << "Please enter in the correct format!" << endl;
+								cout << "Due date: (MM/DD/YYYY)" << endl;
+								fflush(stdin);
+								fseek(stdin, 0, SEEK_END);
+							}
+						}
+						list[taskNum - 1].day.day = dd;
+						list[taskNum - 1].day.year = yyyy;
+						list[taskNum - 1].day.month = mm;
+						switch (list[taskNum - 1].day.month) {
+						case 1:
+							numOfDays = 31;
+							break;
+						case 2:
+							if ((list[taskNum - 1].day.year % 4 == 0 && list[taskNum - 1].day.year % 100 != 0) || list[taskNum - 1].day.year % 400 == 0)
+								numOfDays = 29;
+							else numOfDays = 28;
+							break;
+						case 3:
+							numOfDays = 31;
+							break;
+						case 4:
+							numOfDays = 30;
+							break;
+						case 5:
+							numOfDays = 31;
+							break;
+						case 6:
+							numOfDays = 30;
+							break;
+						case 7:
+							numOfDays = 31;
+							break;
+						case 8:
+							numOfDays = 31;
+							break;
+						case 9:
+							numOfDays = 30;
+							break;
+						case 10:
+							numOfDays = 31;
+							break;
+						case 11:
+							numOfDays = 30;
+							break;
+						case 12:
+							numOfDays = 31;
+							break;
+						default:
+							cout << "Invalid month!" << endl;
+							break;
+						}
+						if (list[taskNum - 1].day.day > numOfDays || list[taskNum - 1].day.day < 1) {
+							cin.clear();
+							cout << "Invalid day for the month and year you entered!" << endl;
+							cin.ignore();
+						}
+						if (list[taskNum - 1].day.year < today.wYear) {
+							cout << "Please enter a year within or after " << today.wYear << endl;
+						}
+					}
+					break;
+				case 4:
+					while (list[taskNum - 1].priority > 9 || list[taskNum - 1].priority < 0) {
+						cout << " New Priority: (Enter a number between 0 and 9 --> 0 least importance and 9 most importance) ";
+						cin >> list[taskNum - 1].priority;
+						if (list[taskNum - 1].priority > 9 || list[taskNum - 1].priority < 0) {
+							cout << "Please enter a number between 0 and 9!" << endl;
+						}
 					}
 				}
-				break;
-			case 4:
-				while (list[taskNum - 1].priority > 9 || list[taskNum - 1].priority < 0) {
-					cout << " New Priority: (Enter a number between 0 and 9 --> 0 least importance and 9 most importance) ";
-					cin >> list[taskNum - 1].priority;
-					if (list[taskNum - 1].priority > 9 || list[taskNum - 1].priority < 0) {
-						cout << "Please enter a number between 0 and 9!" << endl;
-					}
-				}
+				cout << "Would you like to make more edits?" << endl;
+				cin >> loop;
 			}
-			cout << "Would you like to make more edits?" << endl;
-			cin >> loop;
 		}
 	}
 }
