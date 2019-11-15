7f7bb6cf097167144954056f616e2aa4a46ff97d
AndreaHabib <54924600+AndreaHabib@users.noreply.github.com>
2019-11-15 10:47:10 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -8,15 +8,10 @@ int main() {
 	task call;
 	call.getData();
 	string loop = "y";
-	while (loop == "y" || loop == "Y" || loop == "Yes" || loop == "YES" || loop == "yes" || loop == "yES" || loop == "YeS") {
+	int choice = 1;
+	while ((loop == "y" || loop == "Y" || loop == "Yes" || loop == "YES" || loop == "yes" || loop == "yES" || loop == "YeS") && choice != -1) {
 		system("CLS");
 		cout << "Welcome to the TO-DO list" << flush << endl;
-		if (counter == 0) {
-			cout << "No tasks active!" << endl;
-		}
-		else {
-			cout << "You currently have " << counter << " task!" << endl;
-		}
 		cout << "Please choose one of ther following:" << endl;
 		cout << "1. Add a new task" << endl;
 		cout << "2. Mark task as done" << endl;
@@ -28,61 +23,68 @@ int main() {
 		cout << "8. Display all tasks that needs to be done today" << endl;
 		cout << "9. Search for specific task (using Title, Task Number, date)" << endl;
 		int choice = 11;
-		while (!cin || choice > 10 || choice < 1) {
-			cout << "Please enter a number between 1 and 9: ";
+		while (!cin || choice > 10 || choice < 1 && choice != -1) {
+			cout << "Please enter a number between 1 and 9: (-1 to exit) ";
 			cin >> choice;
-			if (cin && choice > 10 || choice < 1) {
+			if (cin && choice > 10 || choice < 1 && choice != -1) {
 				cin.clear();
 				cout << "Invalid! Please enter a valid number!" << endl;
 				cin.ignore();
 			}
 		}
-		switch (choice) {
-		case 1:
-			call.addTask();
-			break;
-		case 2:
-			call.taskDone();
-			break;
-		case 3:
-			call.edit();
-			break;
-		case 4:
-			call.Delete();
-			break;
-		case 5:
-			call.displayAll();
-			break;
-		case 6:
-			call.displayDone();
-			break;
-		case 7:
-			call.displayOverdue();
-			break;
-		case 8:
-			call.displayDueToday();
-			break;
-		case 9:
-			call.search();
-			break;
-		default:
-			exit(1);
-		}
-		loop = "k";
-		while (loop != "y" && loop != "n" && loop != "Y" && loop != "Yes" &&
-			loop != "YES" && loop != "yes" && loop != "yES" && loop != "YeS" && loop != "No" && loop != "NO" &&
-			loop != "nO" && loop != "N") {
-			cout << "Would you like to choose another task?" << endl;
-			cin >> loop;
-			if (loop != "y" && loop != "n" && loop != "Y" && loop != "Yes" &&
+		if (choice != -1) {
+			switch (choice) {
+			case 1:
+				call.addTask();
+				break;
+			case 2:
+				call.taskDone();
+				break;
+			case 3:
+				call.edit();
+				break;
+			case 4:
+				call.Delete();
+				break;
+			case 5:
+				call.displayAll();
+				break;
+			case 6:
+				call.displayDone();
+				break;
+			case 7:
+				call.displayOverdue();
+				break;
+			case 8:
+				call.displayDueToday();
+				break;
+			case 9:
+				call.search();
+				break;
+			}
+			loop = "k";
+			while (loop != "y" && loop != "n" && loop != "Y" && loop != "Yes" &&
 				loop != "YES" && loop != "yes" && loop != "yES" && loop != "YeS" && loop != "No" && loop != "NO" &&
 				loop != "nO" && loop != "N") {
-				cout << "Please enter Y or N!" << endl;
+				cout << "Would you like to choose another task?" << endl;
+				cin >> loop;
+				if (loop != "y" && loop != "n" && loop != "Y" && loop != "Yes" &&
+					loop != "YES" && loop != "yes" && loop != "yES" && loop != "YeS" && loop != "No" && loop != "NO" &&
+					loop != "nO" && loop != "N") {
+					cout << "Please enter Y or N!" << endl;
+				}
 			}
 		}
-
 	}
 	call.outData();
+	for (int j = 0; j < 3; j++) {
+		cout << "\rExiting   \rExiting";
+		for (int i = 0; i < 3; i++) {
+			cout << ".";
+			Sleep(150);
+		}
+	}
+	cout << endl;
 	system("PAUSE");
 	return 0;
 }
@@ -92,26 +94,26 @@ int task::getData() {
 	inData.open("todo.txt");
 	taskData.open("tasknumber.txt");
 	taskData >> counter;
-		if (!inData) {
-			exit(1);
-		} //adding while(!EOF) - title doesn't get read in for first task only
-		//but doesn't read title of second task
-		if (counter > MAX) {
-			call.expandTasks();
-		}
-		for (int taskNum = 0; taskNum < counter; taskNum++) {
-			getline(inData, list[taskNum].title);
-			getline(inData, list[taskNum].detail);
-			inData >> list[taskNum].day.month;
-			inData >> list[taskNum].day.day;
-			inData >> list[taskNum].day.year;
-			inData >> list[taskNum].priority;
-			inData >> list[taskNum].finished;
-			inData >> list[taskNum].overdue;
-			inData.ignore();
-		}
-		inData.close();
-		taskData.close();
+	if (!inData) {
+		exit(1);
+	} //adding while(!EOF) - title doesn't get read in for first task only
+	//but doesn't read title of second task
+	if (counter > MAX) {
+		call.expandTasks();
+	}
+	for (int taskNum = 0; taskNum < counter; taskNum++) {
+		getline(inData, list[taskNum].title);
+		getline(inData, list[taskNum].detail);
+		inData >> list[taskNum].day.month;
+		inData >> list[taskNum].day.day;
+		inData >> list[taskNum].day.year;
+		inData >> list[taskNum].priority;
+		inData >> list[taskNum].finished;
+		inData >> list[taskNum].overdue;
+		inData.ignore();
+	}
+	inData.close();
+	taskData.close();
 	return counter;
 }
 void task::outData() {
